e55d6483dc557ac7bf670890a868ef456864160d
Craig P. Motlin <cmotlin@gmail.com>
2020-12-18 16:45:28 -0500

    Fix MDC leak in liftwizard-graphql-instrumentation-logging.

--- liftwizard-graphql/liftwizard-graphql-instrumentation-logging/src/main/java/io/liftwizard/graphql/instrumentation/logging/LiftwizardGraphQLLoggingInstrumentation.java
+++ liftwizard-graphql/liftwizard-graphql-instrumentation-logging/src/main/java/io/liftwizard/graphql/instrumentation/logging/LiftwizardGraphQLLoggingInstrumentation.java
@@ -16,121 +16,15 @@
 
 package io.liftwizard.graphql.instrumentation.logging;
 
-import java.util.List;
-
-import graphql.ExecutionResult;
-import graphql.execution.ExecutionId;
-import graphql.execution.instrumentation.InstrumentationContext;
 import graphql.execution.instrumentation.SimpleInstrumentation;
-import graphql.execution.instrumentation.parameters.InstrumentationExecutionParameters;
 import graphql.execution.instrumentation.parameters.InstrumentationFieldFetchParameters;
-import graphql.execution.instrumentation.parameters.InstrumentationValidationParameters;
-import graphql.language.Document;
 import graphql.schema.DataFetcher;
 import graphql.schema.GraphQLType;
-import graphql.validation.ValidationError;
 import io.liftwizard.instrumentation.GraphQLInstrumentationUtils;
-import io.liftwizard.logging.slf4j.mdc.MultiMDCCloseable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class LiftwizardGraphQLLoggingInstrumentation
         extends SimpleInstrumentation
 {
-    private static final Logger LOGGER = LoggerFactory.getLogger(LiftwizardGraphQLLoggingInstrumentation.class);
-
-    @Override
-    public InstrumentationContext<ExecutionResult> beginExecution(InstrumentationExecutionParameters parameters)
-    {
-        try
-        {
-            ExecutionId       executionId = parameters.getExecutionInput().getExecutionId();
-            MultiMDCCloseable mdc         = new MultiMDCCloseable();
-            mdc.put("liftwizard.graphql.executionId", executionId.toString());
-
-            return new MDCInstrumentationContext<>(mdc);
-        }
-        catch (RuntimeException e)
-        {
-            LOGGER.warn("", e);
-            throw e;
-        }
-    }
-
-    @Override
-    public InstrumentationContext<Document> beginParse(InstrumentationExecutionParameters parameters)
-    {
-        try
-        {
-            MultiMDCCloseable mdc       = new MultiMDCCloseable();
-            String            operation = parameters.getOperation();
-            if (operation != null)
-            {
-                mdc.put("liftwizard.graphql.operation", operation);
-            }
-            return new MDCInstrumentationContext<>(mdc);
-        }
-        catch (RuntimeException e)
-        {
-            LOGGER.warn("", e);
-            throw e;
-        }
-    }
-
-    @Override
-    public InstrumentationContext<List<ValidationError>> beginValidation(InstrumentationValidationParameters parameters)
-    {
-        try
-        {
-            MultiMDCCloseable mdc       = new MultiMDCCloseable();
-            String            operation = parameters.getOperation();
-            if (operation != null)
-            {
-                mdc.put("liftwizard.graphql.operation", operation);
-            }
-
-            return new MDCInstrumentationContext<>(mdc);
-        }
-        catch (RuntimeException e)
-        {
-            LOGGER.warn("", e);
-            throw e;
-        }
-    }
-
-    @Override
-    public InstrumentationContext<Object> beginFieldFetch(InstrumentationFieldFetchParameters parameters)
-    {
-        try
-        {
-            if (parameters.isTrivialDataFetcher())
-            {
-                return super.beginFieldFetch(parameters);
-            }
-
-            var         mdc            = new MultiMDCCloseable();
-            var         stepInfo       = parameters.getExecutionStepInfo();
-            String      path           = GraphQLInstrumentationUtils.getPathWithIndex(stepInfo);
-            GraphQLType parentType     = stepInfo.getParent().getType();
-            String      parentTypeName = GraphQLInstrumentationUtils.getTypeName(parentType);
-            String      fieldName      = parameters.getField().getName();
-            GraphQLType fieldType      = parameters.getField().getType();
-            String      fieldTypeName  = GraphQLInstrumentationUtils.getTypeName(fieldType);
-
-            mdc.put("liftwizard.graphql.field.path", path);
-            mdc.put("liftwizard.graphql.field.parentType", parentTypeName);
-            mdc.put("liftwizard.graphql.field.name", fieldName);
-            mdc.put("liftwizard.graphql.field.type", fieldTypeName);
-
-            return new MDCInstrumentationContext<>(mdc);
-        }
-        catch (RuntimeException e)
-        {
-            LOGGER.warn("", e);
-            throw e;
-        }
-    }
-
     @Override
     public DataFetcher<?> instrumentDataFetcher(
             DataFetcher<?> dataFetcher,
@@ -141,14 +35,15 @@ public class LiftwizardGraphQLLoggingInstrumentation
             return super.instrumentDataFetcher(dataFetcher, parameters);
         }
 
-        try
-        {
-            return new MDCDataFetcher<>(dataFetcher);
-        }
-        catch (RuntimeException e)
-        {
-            LOGGER.warn("", e);
-            throw e;
-        }
+        var         executionId    = parameters.getExecutionContext().getExecutionId().toString();
+        var         stepInfo       = parameters.getExecutionStepInfo();
+        String      path           = GraphQLInstrumentationUtils.getPathWithIndex(stepInfo);
+        GraphQLType parentType     = stepInfo.getParent().getType();
+        String      parentTypeName = GraphQLInstrumentationUtils.getTypeName(parentType);
+        String      fieldName      = parameters.getField().getName();
+        GraphQLType fieldType      = parameters.getField().getType();
+        String      fieldTypeName  = GraphQLInstrumentationUtils.getTypeName(fieldType);
+
+        return new MDCDataFetcher<>(dataFetcher, executionId, path, parentTypeName, fieldName, fieldTypeName);
     }
 }
