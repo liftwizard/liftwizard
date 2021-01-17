71997ed23e620abbf7e7d2f7427c2b1bac23da2b
Craig P. Motlin <cmotlin@gmail.com>
2021-01-17 17:20:40 -0500

    Add module liftwizard-config-logging-filter-janino.

---
+++ liftwizard-logging/liftwizard-config-logging-filter-janino/src/test/java/io/liftwizard/dropwizard/configuration/logging/filter/janino/JaninoFilterFactoryTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2021 Craig Motlin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.liftwizard.dropwizard.configuration.logging.filter.janino;
+
+import java.io.File;
+import java.net.URL;
+
+import javax.validation.Validator;
+
+import ch.qos.logback.classic.boolex.JaninoEventEvaluator;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.boolex.EventEvaluator;
+import ch.qos.logback.core.filter.EvaluatorFilter;
+import ch.qos.logback.core.filter.Filter;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.Resources;
+import io.dropwizard.configuration.JsonConfigurationFactory;
+import io.dropwizard.jackson.DiscoverableSubtypeResolver;
+import io.dropwizard.jackson.Jackson;
+import io.dropwizard.jersey.validation.Validators;
+import io.liftwizard.junit.rule.log.marker.LogMarkerTestRule;
+import io.liftwizard.serialization.jackson.config.ObjectMapperConfig;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class JaninoFilterFactoryTest
+{
+    @Rule
+    public final TestRule logMarkerTestRule = new LogMarkerTestRule();
+
+    private final ObjectMapper objectMapper = newObjectMapper();
+    private final Validator    validator    = Validators.newValidator();
+
+    private final JsonConfigurationFactory<JaninoFilterFactory> factory = new JsonConfigurationFactory<>(
+            JaninoFilterFactory.class,
+            this.validator,
+            this.objectMapper,
+            "dw");
+
+    @Test
+    public void isDiscoverable()
+    {
+        // Make sure the types we specified in META-INF gets picked up
+        DiscoverableSubtypeResolver discoverableSubtypeResolver = new DiscoverableSubtypeResolver();
+        ImmutableList<Class<?>>     discoveredSubtypes          = discoverableSubtypeResolver.getDiscoveredSubtypes();
+        assertThat(discoveredSubtypes, hasItem(JaninoFilterFactory.class));
+    }
+
+    @Test
+    public void filterJanino() throws Exception
+    {
+        URL                   resource            = Resources.getResource("config-test.json5");
+        File                  json                = new File(resource.toURI());
+        JaninoFilterFactory   janinoFilterFactory = this.factory.build(json);
+        Filter<ILoggingEvent> filter              = janinoFilterFactory.build();
+
+        assertThat(janinoFilterFactory, instanceOf(JaninoFilterFactory.class));
+        assertThat(filter, instanceOf(EvaluatorFilter.class));
+
+        EventEvaluator<?> evaluator = ((EvaluatorFilter<?>) filter).getEvaluator();
+        assertThat(evaluator, instanceOf(JaninoEventEvaluator.class));
+    }
+
+    private static ObjectMapper newObjectMapper()
+    {
+        ObjectMapper objectMapper = Jackson.newObjectMapper();
+        ObjectMapperConfig.configure(objectMapper, true, Include.NON_ABSENT);
+        return objectMapper;
+    }
+}
