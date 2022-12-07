61bfa90635bf13bb9f388bbc86da378408a126db
Craig P. Motlin <cmotlin@gmail.com>
2022-12-07 09:53:33 -0500

    Simplify Liftwizard bundles by clearing all MDC without reporting which MDC keys need to be cleared.

--- liftwizard-auth/liftwizard-config-auth-filter-impersonation/src/main/java/io/liftwizard/dropwizard/configuration/auth/filter/impersonation/ImpersonationAuthenticator.java
+++ liftwizard-auth/liftwizard-config-auth-filter-impersonation/src/main/java/io/liftwizard/dropwizard/configuration/auth/filter/impersonation/ImpersonationAuthenticator.java
@@ -21,26 +21,14 @@ import java.util.Optional;
 import javax.annotation.Nonnull;
 
 import io.dropwizard.auth.Authenticator;
-import org.eclipse.collections.api.factory.Lists;
-import org.eclipse.collections.api.list.ImmutableList;
-import org.slf4j.MDC;
 
-public class ImpersonationAuthenticator implements Authenticator<String, ImpersonatedPrincipal>
+public class ImpersonationAuthenticator
+        implements Authenticator<String, ImpersonatedPrincipal>
 {
-    private static final String IMPERSONATION_PRINCIPAL_NAME = "liftwizard.auth.impersonation.principalName";
-
-    private static final ImmutableList<String> MDC_KEYS = Lists.immutable.with(IMPERSONATION_PRINCIPAL_NAME);
-
-    public static ImmutableList<String> getMDCKeys()
-    {
-        return MDC_KEYS;
-    }
-
     @Nonnull
     @Override
     public Optional<ImpersonatedPrincipal> authenticate(String principalName)
     {
-        MDC.put(IMPERSONATION_PRINCIPAL_NAME, principalName);
         ImpersonatedPrincipal impersonatedUser = new ImpersonatedPrincipal(principalName);
         return Optional.of(impersonatedUser);
     }
