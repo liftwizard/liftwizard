6eb8e26859f44ede3cb29065ea2913fe861cae03
Craig P. Motlin <cmotlin@gmail.com>
2023-03-24 11:42:38 -0400

    Add logging of execution durations to liftwizard prioritized bundles.

--- liftwizard-bundle/liftwizard-bundle-prioritized-api/src/main/java/io/liftwizard/dropwizard/bundle/prioritized/PrioritizedBundle.java
+++ liftwizard-bundle/liftwizard-bundle-prioritized-api/src/main/java/io/liftwizard/dropwizard/bundle/prioritized/PrioritizedBundle.java
@@ -16,17 +16,24 @@
 
 package io.liftwizard.dropwizard.bundle.prioritized;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import javax.annotation.Nonnull;
 
 import io.dropwizard.ConfiguredBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.slf4j.MDC.MDCCloseable;
 
 public interface PrioritizedBundle
         extends ConfiguredBundle<Object>
 {
+    Logger LOGGER = LoggerFactory.getLogger(PrioritizedBundle.class);
+
     String MDC_BUNDLE   = "liftwizard.bundle";
     String MDC_PRIORITY = "liftwizard.priority";
 
@@ -52,12 +59,16 @@ public interface PrioritizedBundle
     @Override
     default void initialize(@Nonnull Bootstrap<?> bootstrap)
     {
+        Instant start = Instant.now();
         try (
                 MDCCloseable mdc1 = MDC.putCloseable(MDC_BUNDLE, this.getClass().getSimpleName());
                 MDCCloseable mdc2 = MDC.putCloseable(MDC_PRIORITY, String.valueOf(this.getPriority())))
         {
             this.initializeWithMdc(bootstrap);
         }
+        Instant end = Instant.now();
+        Duration duration = Duration.between(start, end);
+        LOGGER.info("{} initialized in {}", this.getClass().getSimpleName(), duration);
     }
 
     default void initializeWithMdc(@Nonnull Bootstrap<?> bootstrap)
@@ -67,12 +78,16 @@ public interface PrioritizedBundle
     @Override
     default void run(@Nonnull Object configuration, @Nonnull Environment environment) throws Exception
     {
+        Instant start = Instant.now();
         try (
                 MDCCloseable mdc1 = MDC.putCloseable(MDC_BUNDLE, this.getClass().getSimpleName());
                 MDCCloseable mdc2 = MDC.putCloseable(MDC_PRIORITY, String.valueOf(this.getPriority())))
         {
             this.runWithMdc(configuration, environment);
         }
+        Instant end = Instant.now();
+        Duration duration = Duration.between(start, end);
+        LOGGER.info("{} ran in {}", this.getClass().getSimpleName(), duration);
     }
 
     void runWithMdc(@Nonnull Object configuration, @Nonnull Environment environment) throws Exception;
