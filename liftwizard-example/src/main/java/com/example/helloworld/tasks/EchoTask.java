6a8cba24154b9e4a9814bb94937709f4f382b05f
Craig P. Motlin <cmotlin@gmail.com>
2023-05-19 16:12:25 -0400

    Upgrade Dropwizard from 1.3.29 to 2.0.35.  Bump antlr4-maven-plugin from 4.9.3 to 4.12.0.  Bump antlr4-runtime from 4.9.3 to 4.12.0.  Upgrade graphql-java from 15.0 to 20.0, including breaking changes from the underlying library.

--- liftwizard-example/src/main/java/com/example/helloworld/tasks/EchoTask.java
+++ liftwizard-example/src/main/java/com/example/helloworld/tasks/EchoTask.java
@@ -1,8 +1,9 @@
 package com.example.helloworld.tasks;
 
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
 
-import com.google.common.collect.ImmutableMultimap;
 import io.dropwizard.servlets.tasks.PostBodyTask;
 
 public class EchoTask extends PostBodyTask {
@@ -11,7 +12,7 @@ public class EchoTask extends PostBodyTask {
     }
 
     @Override
-    public void execute(ImmutableMultimap<String, String> parameters, String body, PrintWriter output) throws Exception {
+    public void execute(Map<String, List<String>> parameters, String body, PrintWriter output) {
         output.print(body);
         output.flush();
     }
