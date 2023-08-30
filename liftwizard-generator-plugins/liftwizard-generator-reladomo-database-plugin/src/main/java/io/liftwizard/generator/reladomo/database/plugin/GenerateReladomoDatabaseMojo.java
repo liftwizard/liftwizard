240a76474cae3c3533885a8e36ad20c06959a715
Craig P. Motlin <cmotlin@gmail.com>
2023-08-29 21:55:43 -0400

    Change Reladomo code generators that use FileSystems to try to reuse existing FileSystems when possible to avoid concurrency issues.

--- liftwizard-generator-plugins/liftwizard-generator-reladomo-database-plugin/src/main/java/io/liftwizard/generator/reladomo/database/plugin/GenerateReladomoDatabaseMojo.java
+++ liftwizard-generator-plugins/liftwizard-generator-reladomo-database-plugin/src/main/java/io/liftwizard/generator/reladomo/database/plugin/GenerateReladomoDatabaseMojo.java
@@ -22,6 +22,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.FileSystem;
+import java.nio.file.FileSystemNotFoundException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -129,7 +130,7 @@ public class GenerateReladomoDatabaseMojo
             URL resource = this.getClass().getResource("/" + this.definitionsAndClassListDirectory);
             Objects.requireNonNull(resource, () -> "Could not find /" + this.definitionsAndClassListDirectory);
             URI uri = resource.toURI();
-            try (FileSystem fileSystem = FileSystems.newFileSystem(uri, Map.of()))
+            try (FileSystem fileSystem = getFileSystem(uri))
             {
                 Path from = fileSystem.getPath("/");
                 Path to   = Files.createTempDirectory(this.getClass().getSimpleName());
@@ -143,6 +144,26 @@ public class GenerateReladomoDatabaseMojo
         catch (URISyntaxException | IOException e)
         {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static FileSystem getFileSystem(URI uri)
+            throws IOException
+    {
+        try
+        {
+            return FileSystems.getFileSystem(uri);
+        }
+        catch (FileSystemNotFoundException notFoundException)
+        {
+            try
+            {
+                return FileSystems.newFileSystem(uri, Map.of());
+            }
+            catch (java.nio.file.FileSystemAlreadyExistsException alreadyExistsException)
+            {
+                return FileSystems.getFileSystem(uri);
+            }
         }
     }
 
