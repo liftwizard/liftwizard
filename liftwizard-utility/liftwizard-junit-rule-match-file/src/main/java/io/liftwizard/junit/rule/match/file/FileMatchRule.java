aa803963819f6012727628d9c9a951c3dc116983
Craig P. Motlin <cmotlin@gmail.com>
2023-08-31 10:34:42 -0400

    Enhance FileMatchRule to be able to sync test resources directories.

--- liftwizard-utility/liftwizard-junit-rule-match-file/src/main/java/io/liftwizard/junit/rule/match/file/FileMatchRule.java
+++ liftwizard-utility/liftwizard-junit-rule-match-file/src/main/java/io/liftwizard/junit/rule/match/file/FileMatchRule.java
@@ -18,21 +18,28 @@ package io.liftwizard.junit.rule.match.file;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.Scanner;
 
 import javax.annotation.Nonnull;
 
+import org.eclipse.collections.api.factory.Sets;
 import org.eclipse.collections.api.list.ListIterable;
+import org.eclipse.collections.api.set.MutableSet;
 import org.eclipse.collections.impl.list.fixed.ArrayAdapter;
 import org.junit.rules.ErrorCollector;
 
@@ -42,6 +49,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class FileMatchRule
         extends ErrorCollector
 {
+    private static final MutableSet<Path> CLEANED_PATHS = Sets.mutable.empty();
+
+    @Nonnull
+    private final Class<?> callingClass;
+    private final boolean  rerecordEnabled;
+
+    public FileMatchRule(@Nonnull Class<?> callingClass)
+    {
+        this.callingClass    = Objects.requireNonNull(callingClass);
+        this.rerecordEnabled = Boolean.parseBoolean(System.getenv("LIFTWIZARD_FILE_MATCH_RULE_RERECORD"));
+    }
+
     public static String slurp(@Nonnull String resourceClassPathLocation, @Nonnull Class<?> callingClass)
     {
         return FileMatchRule.slurp(resourceClassPathLocation, callingClass, StandardCharsets.UTF_8);
@@ -67,18 +86,17 @@ public class FileMatchRule
 
     public void assertFileContents(
             @Nonnull String resourceClassPathLocation,
-            @Nonnull String actualString,
-            @Nonnull Class<?> callingClass)
+            @Nonnull String actualString)
     {
         try
         {
-            this.assertFileContentsOrThrow(resourceClassPathLocation, actualString, callingClass);
+            this.assertFileContentsOrThrow(resourceClassPathLocation, actualString);
         }
-        catch (@Nonnull URISyntaxException | FileNotFoundException e)
+        catch (@Nonnull URISyntaxException e)
         {
             throw new RuntimeException(e);
         }
-        catch (NoSuchElementException e)
+        catch (IOException | NoSuchElementException e)
         {
             throw new RuntimeException(resourceClassPathLocation, e);
         }
@@ -86,27 +104,31 @@ public class FileMatchRule
 
     private void assertFileContentsOrThrow(
             @Nonnull String resourceClassPathLocation,
-            @Nonnull String actualString,
-            @Nonnull Class<?> callingClass)
-            throws URISyntaxException, FileNotFoundException
+            @Nonnull String actualString)
+            throws URISyntaxException, IOException
     {
-        InputStream inputStream = callingClass.getResourceAsStream(resourceClassPathLocation);
-        if (inputStream == null)
+        Path packagePath = FileMatchRule.getPackagePath(this.callingClass);
+        if (this.rerecordEnabled && !this.CLEANED_PATHS.contains(packagePath))
         {
-            String               packageName      = callingClass.getPackage().getName();
-            ListIterable<String> packageNameParts = ArrayAdapter.adapt(packageName.split("\\."));
-            Path                 testResources    = Paths.get("", "src", "test", "resources").toAbsolutePath();
-            Path                 packagePath      = packageNameParts.injectInto(testResources, Path::resolve);
-            File                 resourceFile     = packagePath.resolve(resourceClassPathLocation).toFile();
+            FileMatchRule.deleteDirectoryRecursively(packagePath);
+            this.CLEANED_PATHS.add(packagePath);
+        }
+        InputStream inputStream = this.callingClass.getResourceAsStream(resourceClassPathLocation);
+        if (this.rerecordEnabled || inputStream == null)
+        {
+            File resourceFile = packagePath.resolve(resourceClassPathLocation).toFile();
 
             assertThat(resourceFile.getAbsolutePath(), resourceFile.exists(), is(false));
             this.writeStringToFile(actualString, resourceFile);
-            this.addError(new AssertionError(resourceClassPathLocation + " did not exist. Created it."));
+            if (!this.rerecordEnabled)
+            {
+                this.addError(new AssertionError(resourceClassPathLocation + " did not exist. Created it."));
+            }
         }
         else
         {
             String expectedStringFromFile = FileMatchRule.slurp(inputStream, StandardCharsets.UTF_8);
-            URI    uri                    = callingClass.getResource(resourceClassPathLocation).toURI();
+            URI    uri                    = this.callingClass.getResource(resourceClassPathLocation).toURI();
             if (!actualString.equals(expectedStringFromFile))
             {
                 File file = new File(uri);
@@ -117,6 +139,42 @@ public class FileMatchRule
                     actualString,
                     is(expectedStringFromFile));
         }
+    }
+
+    @Nonnull
+    private static void deleteDirectoryRecursively(@Nonnull Path directory)
+            throws IOException
+    {
+        if (!directory.toFile().exists())
+        {
+            return;
+        }
+        Files.walkFileTree(directory, new SimpleFileVisitor<>()
+        {
+            @Override
+            public FileVisitResult visitFile(@Nonnull Path file, @Nonnull BasicFileAttributes attrs)
+                    throws IOException
+            {
+                Files.delete(file);
+                return super.visitFile(file, attrs);
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(@Nonnull Path dir, IOException exc)
+                    throws IOException
+            {
+                Files.delete(dir);
+                return super.postVisitDirectory(dir, exc);
+            }
+        });
+    }
+
+    private static Path getPackagePath(@Nonnull Class<?> callingClass)
+    {
+        String               packageName      = callingClass.getPackage().getName();
+        ListIterable<String> packageNameParts = ArrayAdapter.adapt(packageName.split("\\."));
+        Path                 testResources    = Paths.get("", "src", "test", "resources").toAbsolutePath();
+        return packageNameParts.injectInto(testResources, Path::resolve);
     }
 
     private void writeStringToFile(@Nonnull String string, @Nonnull File file)
