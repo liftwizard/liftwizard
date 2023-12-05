0fcc677ad5a2420af2d936410f16ff8124677142
Craig P. Motlin <cmotlin@gmail.com>
2023-12-05 09:18:23 -0500

    Update JsonPrettyPrinter to format empty arrays as `[]` instead of `[ ]`.

---
+++ liftwizard-jackson/liftwizard-jackson-pretty-printer/src/test/java/io/liftwizard/serialization/jackson/pretty/JsonPrettyPrinterTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2023 Craig Motlin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.liftwizard.serialization.jackson.pretty;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class JsonPrettyPrinterTest
+{
+    private final ObjectMapper mapper = JsonPrettyPrinterTest.getObjectMapper();
+
+    @Nonnull
+    private static ObjectMapper getObjectMapper()
+    {
+        JsonPrettyPrinter jsonPrettyPrinter = new JsonPrettyPrinter();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setDefaultPrettyPrinter(jsonPrettyPrinter);
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        return objectMapper;
+    }
+
+    @Test
+    public void smokeTest()
+            throws JsonProcessingException
+    {
+        Map<String, List<String>> map = new HashMap<>();
+        map.put("a", List.of("b", "c"));
+        map.put("d", List.of("e", "f"));
+
+        String actualJson = this.mapper.writeValueAsString(map);
+
+        //language=JSON
+        String expectedJson = """
+                {
+                  "a": [
+                    "b",
+                    "c"
+                  ],
+                  "d": [
+                    "e",
+                    "f"
+                  ]
+                }""";
+
+        assertEquals(
+                expectedJson,
+                actualJson);
+    }
+
+    @Test
+    public void emptyArray()
+            throws JsonProcessingException
+    {
+        List<String> emptyList    = List.of();
+        String       actualJson   = this.mapper.writeValueAsString(emptyList);
+        String       expectedJson = "[]";
+        assertEquals(expectedJson, actualJson);
+    }
+}
