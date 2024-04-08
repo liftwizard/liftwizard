1ea6f441714a08938c642c977b1b22e9f40c36d9
Craig P. Motlin <cmotlin@gmail.com>
2024-04-08 07:41:27 -0400

    Upgrade liftwizard-example from JUnit 4 to JUnit 5.

--- liftwizard-example/src/test/java/com/example/helloworld/IntegrationTest.java
+++ liftwizard-example/src/test/java/com/example/helloworld/IntegrationTest.java
@@ -14,60 +14,55 @@ import javax.ws.rs.core.Response.Status;
 
 import com.example.helloworld.dto.PersonDTO;
 import io.dropwizard.testing.ResourceHelpers;
-import io.dropwizard.testing.junit.DropwizardAppRule;
-import io.liftwizard.junit.rule.log.marker.LogMarkerTestRule;
-import io.liftwizard.reladomo.test.rule.ReladomoInitializeTestRule;
-import io.liftwizard.reladomo.test.rule.ReladomoPurgeAllTestRule;
-import org.eclipse.jetty.http.HttpStatus;
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExternalResource;
-import org.junit.rules.RuleChain;
-import org.junit.rules.TestRule;
+import io.liftwizard.junit.extension.app.LiftwizardAppExtension;
+import io.liftwizard.junit.extension.log.marker.LogMarkerTestExtension;
+import io.liftwizard.reladomo.test.extension.ReladomoInitializeExtension;
+import io.liftwizard.reladomo.test.extension.ReladomoLoadDataExtension;
+import io.liftwizard.reladomo.test.extension.ReladomoPurgeAllExtension;
+import org.glassfish.grizzly.http.util.HttpStatus;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.hamcrest.CoreMatchers.is;
 
 public class IntegrationTest {
     private static final String CONFIG_PATH = ResourceHelpers.resourceFilePath("test-example.json5");
 
-    private final DropwizardAppRule<HelloWorldConfiguration> dropwizardAppRule = new DropwizardAppRule<>(
+    @RegisterExtension
+    @Order(1)
+    private final LiftwizardAppExtension<HelloWorldConfiguration> dropwizardAppExtension = new LiftwizardAppExtension<>(
             HelloWorldApplication.class,
             CONFIG_PATH);
 
-    private final ExternalResource dbMigrateRule = new ExternalResource()
-    {
-        @Override
-        protected void before() throws Throwable
-        {
-            IntegrationTest.this.dropwizardAppRule.getApplication().run("db", "migrate", CONFIG_PATH);
-        }
-    };
+    @RegisterExtension
+    @Order(2)
+    private final BeforeEachCallback dbMigrateRule = context -> this.dropwizardAppExtension
+            .getApplication()
+            .run("db", "migrate", CONFIG_PATH);
 
-    private final ReladomoInitializeTestRule initializeTestRule =
-            new ReladomoInitializeTestRule("reladomo-runtime-configuration/ReladomoRuntimeConfiguration.xml");
+    @RegisterExtension
+    @Order(3)
+    private final ReladomoInitializeExtension initializeExtension =
+            new ReladomoInitializeExtension("reladomo-runtime-configuration/ReladomoRuntimeConfiguration.xml");
 
-    private final ReladomoPurgeAllTestRule purgeAllTestRule = new ReladomoPurgeAllTestRule();
+    @RegisterExtension
+    @Order(4)
+    private final ReladomoPurgeAllExtension purgeAllExtension  = new ReladomoPurgeAllExtension();
 
-    private final TestRule logMarkerTestRule = new LogMarkerTestRule();
-
-    @Rule
-    public final RuleChain ruleChain = RuleChain
-            .outerRule(this.dropwizardAppRule)
-            .around(this.dbMigrateRule)
-            .around(this.initializeTestRule)
-            .around(this.purgeAllTestRule)
-            .around(this.logMarkerTestRule);
+    @RegisterExtension
+    @Order(6)
+    private final LogMarkerTestExtension    logMarkerExtension = new LogMarkerTestExtension();
 
     @Test
     public void testHelloWorld() throws Exception {
-        Response response = this.dropwizardAppRule
+        Response response = this.dropwizardAppExtension
                 .client()
-                .target(String.format("http://localhost:%d/hello-world", dropwizardAppRule.getLocalPort()))
+                .target(String.format("http://localhost:%d/hello-world", this.dropwizardAppExtension.getLocalPort()))
                 .queryParam("name", "Dr. IntegrationTest")
                 .request()
                 .get();
@@ -89,12 +84,13 @@ public class IntegrationTest {
     {
         response.bufferEntity();
         String entityAsString = response.readEntity(String.class);
-        Assert.assertThat(entityAsString, response.getStatusInfo(), is(status));
+        assertThat(response.getStatusInfo().toEnum()).as(entityAsString).isEqualTo(status);
     }
 
     @Test
     public void validDateParameter() {
-        final String date = dropwizardAppRule.client().target("http://localhost:" + dropwizardAppRule.getLocalPort() + "/hello-world/date")
+        String date = this.dropwizardAppExtension
+                .client().target("http://localhost:" + this.dropwizardAppExtension.getLocalPort() + "/hello-world/date")
                 .queryParam("date", "2022-01-20")
                 .request()
                 .get(String.class);
@@ -104,7 +100,8 @@ public class IntegrationTest {
     @Test
     public void invalidDateParameter() {
         assertThatExceptionOfType(BadRequestException.class)
-                .isThrownBy(() -> dropwizardAppRule.client().target("http://localhost:" + dropwizardAppRule.getLocalPort() + "/hello-world/date")
+                .isThrownBy(() -> this.dropwizardAppExtension
+                        .client().target("http://localhost:" + this.dropwizardAppExtension.getLocalPort() + "/hello-world/date")
                         .queryParam("date", "abc")
                         .request()
                         .get(String.class));
@@ -112,7 +109,8 @@ public class IntegrationTest {
 
     @Test
     public void noDateParameter() {
-        final String date = dropwizardAppRule.client().target("http://localhost:" + dropwizardAppRule.getLocalPort() + "/hello-world/date")
+        String date = this.dropwizardAppExtension
+                .client().target("http://localhost:" + this.dropwizardAppExtension.getLocalPort() + "/hello-world/date")
                 .request()
                 .get(String.class);
         assertThat(date).isEmpty();
@@ -120,8 +118,8 @@ public class IntegrationTest {
 
     @Test
     public void testPostPerson() {
-        final PersonDTO person = new PersonDTO("Dr. IntegrationTest", "Chief Wizard");
-        final PersonDTO newPerson = this.postPerson(person);
+        PersonDTO person = new PersonDTO("Dr. IntegrationTest", "Chief Wizard");
+        PersonDTO newPerson = this.postPerson(person);
         assertThat(newPerson.getId()).isNotNull();
         assertThat(newPerson.getFullName()).isEqualTo(person.getFullName());
         assertThat(newPerson.getJobTitle()).isEqualTo(person.getJobTitle());
@@ -137,16 +135,18 @@ public class IntegrationTest {
         this.testRenderingPerson("view_mustache");
     }
 
-    private void testRenderingPerson(String viewName) throws Exception {
-        final PersonDTO person    = new PersonDTO( "Dr. IntegrationTest", "Chief Wizard");
-        final PersonDTO    newPerson = this.postPerson(person);
-        final String url = "http://localhost:" + dropwizardAppRule.getLocalPort() + "/people/" + newPerson.getId() + "/" + viewName;
-        Response response = dropwizardAppRule.client().target(url).request().get();
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK_200);
+    private void testRenderingPerson(String viewName)
+    {
+        PersonDTO person    = new PersonDTO( "Dr. IntegrationTest", "Chief Wizard");
+        PersonDTO    newPerson = this.postPerson(person);
+        String url = "http://localhost:" + this.dropwizardAppExtension.getLocalPort() + "/people/" + newPerson.getId() + "/" + viewName;
+        Response response = this.dropwizardAppExtension.client().target(url).request().get();
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK_200.getStatusCode());
     }
 
     private PersonDTO postPerson(PersonDTO person) {
-        return dropwizardAppRule.client().target("http://localhost:" + dropwizardAppRule.getLocalPort() + "/people")
+        return this.dropwizardAppExtension
+                .client().target("http://localhost:" + this.dropwizardAppExtension.getLocalPort() + "/people")
                 .request()
                 .post(Entity.entity(person, MediaType.APPLICATION_JSON_TYPE))
                 .readEntity(PersonDTO.class);
@@ -157,9 +157,9 @@ public class IntegrationTest {
         // The log file is using a size and time based policy, which used to silently
         // fail (and not write to a log file). This test ensures not only that the
         // log file exists, but also contains the log line that jetty prints on startup
-        final Path log = Paths.get("./logs/application.log");
+        Path log = Paths.get("./logs/application.log");
         assertThat(log).exists();
-        final String actual = Files.readString(log);
-        assertThat(actual).contains("0.0.0.0:" + dropwizardAppRule.getLocalPort());
+        String actual = Files.readString(log);
+        assertThat(actual).contains("0.0.0.0:" + this.dropwizardAppExtension.getLocalPort());
     }
 }
