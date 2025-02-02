7e03e11cac2202028a780b7000d9ec1d4d7a5697
Craig P. Motlin <cmotlin@gmail.com>
2025-02-02 18:46:53 -0500

    Refactor liftwizard-config-auth-filter-firebase to allow getting initialized components from getters.

--- liftwizard-auth/liftwizard-config-auth-filter-firebase/src/main/java/io/liftwizard/dropwizard/configuration/auth/filter/firebase/FirebaseAuth.java
+++ liftwizard-auth/liftwizard-config-auth-filter-firebase/src/main/java/io/liftwizard/dropwizard/configuration/auth/filter/firebase/FirebaseAuth.java
@@ -22,16 +22,18 @@ import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 
-import javax.annotation.Nonnull;
-
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class FirebaseAuth
 {
     private final String databaseUrl;
     private final String firebaseConfig;
+
+    private FirebaseApp firebaseApp;
+    private GoogleCredentials credentials;
 
     public FirebaseAuth(String databaseUrl, String firebaseConfig)
     {
@@ -39,37 +41,60 @@ public class FirebaseAuth
         this.firebaseConfig = Objects.requireNonNull(firebaseConfig);
     }
 
-    public com.google.firebase.auth.FirebaseAuth getFirebaseAuth()
+    public GoogleCredentials getCredentials()
     {
-        byte[] bytes = this.firebaseConfig.getBytes(StandardCharsets.UTF_8);
-        InputStream firebaseCredentials = new ByteArrayInputStream(bytes);
-
-        return this.getFirebaseAuth(firebaseCredentials);
+        return this.credentials;
     }
 
-    private com.google.firebase.auth.FirebaseAuth getFirebaseAuth(@Nonnull InputStream firebaseCredentials)
+    public FirebaseApp getFirebaseApp()
     {
+        return this.firebaseApp;
+    }
+
+    public com.google.firebase.auth.FirebaseAuth getFirebaseAuth()
+    {
+        this.initFirebaseApp();
+        return com.google.firebase.auth.FirebaseAuth.getInstance(this.firebaseApp);
+    }
+
+    public FirebaseDatabase getFirebaseDatabase()
+    {
+        this.initFirebaseApp();
+        return FirebaseDatabase.getInstance(this.firebaseApp);
+    }
+
+    private void initFirebaseApp()
+    {
+        if (this.firebaseApp != null)
+        {
+            return;
+        }
+
+        this.initCredentials();
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(this.credentials)
+                .setDatabaseUrl(this.databaseUrl)
+                .build();
+
+        this.firebaseApp = FirebaseApp.initializeApp(options);
+    }
+
+    private void initCredentials()
+    {
+        if (this.credentials != null)
+        {
+            return;
+        }
+
+        byte[] bytes = this.firebaseConfig.getBytes(StandardCharsets.UTF_8);
+        InputStream firebaseCredentials = new ByteArrayInputStream(bytes);
         try
         {
-            return this.getFirebaseAuthNoThrow(firebaseCredentials);
+            this.credentials = GoogleCredentials.fromStream(firebaseCredentials);
         }
         catch (IOException e)
         {
             throw new RuntimeException(e);
         }
-    }
-
-    private com.google.firebase.auth.FirebaseAuth getFirebaseAuthNoThrow(@Nonnull InputStream firebaseCredentials)
-            throws IOException
-    {
-        Objects.requireNonNull(firebaseCredentials);
-        GoogleCredentials credentials = GoogleCredentials.fromStream(firebaseCredentials);
-        FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(credentials)
-                .setDatabaseUrl(this.databaseUrl)
-                .build();
-
-        FirebaseApp firebaseApp = FirebaseApp.initializeApp(options);
-        return com.google.firebase.auth.FirebaseAuth.getInstance(firebaseApp);
     }
 }
