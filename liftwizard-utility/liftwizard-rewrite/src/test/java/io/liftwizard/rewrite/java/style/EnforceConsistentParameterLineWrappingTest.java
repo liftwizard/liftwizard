b9d0057891d3a74385f030b45515a5f2efa33907
Craig P. Motlin <cmotlin@gmail.com>
2026-03-09 11:30:50 -0400

    Add EnforceConsistentParameterLineWrapping recipe to enforce consistent line wrapping in method declaration parameters.

---
+++ liftwizard-utility/liftwizard-rewrite/src/test/java/io/liftwizard/rewrite/java/style/EnforceConsistentParameterLineWrappingTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2025 Craig Motlin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.liftwizard.rewrite.java.style;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.DocumentExample;
+import org.openrewrite.test.RecipeSpec;
+import org.openrewrite.test.RewriteTest;
+
+import static org.openrewrite.java.Assertions.java;
+
+class EnforceConsistentParameterLineWrappingTest implements RewriteTest {
+
+	@Override
+	public void defaults(RecipeSpec spec) {
+		spec.recipe(new EnforceConsistentParameterLineWrapping());
+	}
+
+	@DocumentExample
+	@Test
+	void replacePatterns() {
+		this.rewriteRun(
+				java(
+					"""
+					abstract class Test {
+					    void partiallyWrapped(String a,
+					            String b,
+					            String c) {}
+
+					    void twoParamsPartiallyWrapped(String a,
+					            String b) {}
+
+					    void firstParamWrappedOthersNot(
+					            String a, String b, String c) {}
+
+					    abstract String abstractMethod(String a,
+					            String b);
+					}""",
+					"""
+					abstract class Test {
+					    void partiallyWrapped(
+					            String a,
+					            String b,
+					            String c) {}
+
+					    void twoParamsPartiallyWrapped(
+					            String a,
+					            String b) {}
+
+					    void firstParamWrappedOthersNot(
+					            String a,
+					            String b,
+					            String c) {}
+
+					    abstract String abstractMethod(
+					            String a,
+					            String b);
+					}"""
+				)
+			);
+	}
+
+	@Test
+	void doNotReplaceInvalidPatterns() {
+		this.rewriteRun(
+				java(
+					"""
+					class Test {
+					    void singleLineCall(String a, String b, String c) {}
+
+					    void alreadyFullyWrapped(
+					            String a,
+					            String b,
+					            String c) {}
+
+					    void singleParam(String a) {}
+
+					    void singleParamWrapped(
+					            String a) {}
+
+					    void noParams() {}
+					}"""
+				)
+			);
+	}
+}
