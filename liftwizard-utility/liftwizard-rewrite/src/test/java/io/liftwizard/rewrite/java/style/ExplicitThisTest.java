1d1ea6d60b0956578179deb12d65061781d03c8c
Craig P. Motlin <cmotlin@gmail.com>
2026-03-26 23:17:23 -0400

    Deprecate local ExplicitThis recipe in favor of upstream rewrite-static-analysis.

--- liftwizard-utility/liftwizard-rewrite/src/test/java/io/liftwizard/rewrite/java/style/ExplicitThisTest.java
+++ liftwizard-utility/liftwizard-rewrite/src/test/java/io/liftwizard/rewrite/java/style/ExplicitThisTest.java
@@ -23,6 +23,7 @@ import org.openrewrite.test.RewriteTest;
 
 import static org.openrewrite.java.Assertions.java;
 
+@SuppressWarnings("deprecation")
 class ExplicitThisTest implements RewriteTest {
 
 	@Override
